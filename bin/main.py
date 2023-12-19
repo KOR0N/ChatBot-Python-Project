5947527cf8ba3e9e5b1504f2c1fd82020775c56d
ALEX <152001452+KOR0N@users.noreply.github.com>
2023-12-19 02:38:45 +0100

    Update main.py

--- bin/main.py
+++ bin/main.py
@@ -1,47 +1,73 @@
+#########################################################################################################
+##################################### CHATBOT PROJET PYTHON #############################################
+#########################################################################################################
+# Alexandre LE GALLIARD
+# Alexandre DAUVEL
+
+# Ce fichier comporte le menu du ChatBot en utilisant les fonctions du fichier functions.py
+
+#-------------------------------------------------------------------------------------------------------#
+
+
 from functions import *
 
 dossier = "./speeche/cleaned"
-tf_list = tf_score(dossier)
+
+files_names = list_of_files("./speeche/speeches", "txt")
+
+tf_list = []
+for filename in os.listdir(dossier):
+    fichier_entre = os.path.join(dossier, filename)
+    with open(fichier_entre, "r") as file:
+        content = file.read()
+        content = content.split() 
+        tf_list.append(tf(content))
+
 idf = idf_score(dossier)
 matrice_tf_idf = association_tf_idf(tf_list,idf)
-# print(matrice_tf_idf)
-question = tokenisation("Bonjour messieurs comment ca va tout le monde bienvenue sur mon programme pyton je suis un gars président ?")
-tf_q = tf(question)
-matrice = tf_idf(tf_q,idf)
-print(matrice)
-s = calculer_similarite_cosinus(matrice,matrice_tf_idf)
-s = trouver_document_similaire(s)
-print(s)
+non_important = score_tfidf_plus_faible(matrice_tf_idf, idf)
 
-
-# presence(question, idf)
 if __name__ == "__main__":
-    question = int(input("Voici différentes fonctions disponibles :\n   1) Afficher la liste des mots les moins importants dans le corpus de documents.\n   2) Afficher le(s) mot(s) ayant le score TD-IDF le plus élevé.\n   3) Indiquer le(s) mot(s) le(s) plus répété(s) par un président.\n   4) Indiquer le(s) nom(s) du (des) président(s) qui a (ont) parlé d'un mot à choisir et celui qui l’a répété le plus de fois.\n   5) Indiquer le premier président à parler d'un thème/sujet choisi.\n   6) Hormis les mots dits « non importants », quel(s) est(sont) le(s) mot(s) que tous les présidents ont évoqués.\n   7) QUITTER\nVeuillez choisir une de ces options (1/2/3/4/5/6/7) :"))
-    while question < 1 or question > 7 :
-        question = int(input("ERREUR. Veuillez choisir une option à nouveau : "))
-    match question:
-        case 1:
-            print(score_tfidf_plus_faible(matrice_tf_idf, idf))
-        case 2:
-            mots_max(matrice_tf_idf, idf)
-        case 3:
-            choice = input("\nPour cette fonctionnalité vous devez rechercher un nom de président. Voulez vous voir la liste des présidents archivés ? (o/n) : ")
-            if choice == "o" :
-                presidents_name_display(recup_nom(files_names))
-            elif choice != "o" and choice != "n":
-                while  choice != "o" and choice != "n" :
-                    choice = input("ERREUR. Pour cette fonctionnalité vous devez rechercher un nom de président. Voulez vous voir la liste des présidents archivés ? (o/n) : ")
-            
-            president_name = input("\nVeuillez entrer un nom de président : ")
-            while president_name not in recup_nom(files_names):
-                president_name = input("Nom non valide. Veuillez entrer un nom de président : ")
-            mots_plus_répétés_par_président(president_name,tf_list)
-        case 4:
-            mot = input("\nVeuillez entrer un mot à rechercher : ")
-            mots_dit_par_présidents(mot,tf_list)
-        case 5:
-            theme = input("\nVeuillez entrer un theme à rechercher : ")
-            premier_president_dire_theme(theme,tf_list)
-        case 6:
-            mots_dit_par_tous_les_presidents(matrice_tf_idf,tf_list)
+    question = int(input("Parmi ces fonctionnalités :\n    1) Accéder aux fonctionnalités proposées par notre application.\n    2) Accéder au ChatBot.\nVeuillez en choisir une (1/2) : "))
+    while question != 1 and question != 2 :
+        question = int(input("Parmi ces fonctionnalités :\n    1) Accéder aux fonctionnalités proposées par notre application.\n    2) Accéder au ChatBot.\nVeuillez en choisir une (1/2) : "))
 
+    if question == 1 :
+        question = int(input("\nVoici différentes fonctions disponibles :\n   1) Afficher la liste des mots les moins importants dans le corpus de documents.\n   2) Afficher le(s) mot(s) ayant le score TD-IDF le plus élevé.\n   3) Indiquer le(s) mot(s) le(s) plus répété(s) par un président.\n   4) Indiquer le(s) nom(s) du (des) président(s) qui a (ont) parlé d'un mot à choisir et celui qui l’a répété le plus de fois.\n   5) Indiquer le premier président à parler d'un thème/sujet choisi.\n   6) Hormis les mots dits « non importants », quel(s) est(sont) le(s) mot(s) que tous les présidents ont évoqués.\n   7) QUITTER\nVeuillez choisir une de ces options (1/2/3/4/5/6/7) : "))
+        while question < 1 or question > 7 :
+            question = int(input("ERREUR. Veuillez choisir une option à nouveau : "))
+        match question:
+            case 1:
+                print(non_important)
+            case 2:
+                mots_max(matrice_tf_idf, idf)
+            case 3:
+                choice = input("\nPour cette fonctionnalité vous devez rechercher un nom de président. Voulez vous voir la liste des présidents archivés ? (o/n) : ")
+                if choice == "o" :
+                    presidents_name_display(recup_nom(files_names))
+                elif choice != "o" and choice != "n":
+                    while  choice != "o" and choice != "n" :
+                        choice = input("ERREUR. Pour cette fonctionnalité vous devez rechercher un nom de président. Voulez vous voir la liste des présidents archivés ? (o/n) : ")
+                
+                president_name = input("\nVeuillez entrer un nom de président : ")
+                while president_name not in recup_nom(files_names):
+                    president_name = input("Nom non valide. Veuillez entrer un nom de président : ")
+                mots_plus_répétés_par_président(president_name,tf_list,files_names)
+            case 4:
+                mot = input("\nVeuillez entrer un mot à rechercher : ")
+                mots_dit_par_présidents(mot,tf_list,files_names)
+            case 5:
+                theme = input("\nVeuillez entrer un theme à rechercher : ")
+                premier_president_dire_theme(theme,tf_list,files_names)
+            case 6:
+                mots_dit_par_tous_les_presidents(idf,tf_list,non_important)
+
+    else:
+        question = tokenisation(input("\nEntrez une question pour le ChatBot : "))
+        tf_q = tf(question)
+        m = tf_idf_question(tf_q, idf,files_names)
+        matrice = inverse_matrice(matrice_tf_idf)
+        final = similarite(m,matrice)
+        doc_plus_pertinent = plus_pertinent(final)
+
+        print(f'Le document le plus similaire à cette question est le document : "{files_names[doc_plus_pertinent]}"')
